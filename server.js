8ba68c069670982699f0222d0521f0c1f7345315
nur farazi <nur369188@gmail.com>
2015-12-03 14:12:41 +0600

    server js

--- server.js
+++ server.js
@@ -25,6 +25,9 @@ app.use(bodyParser.json());
 // use morgan to log requests to the console
 app.use(morgan('dev'));
 
+
+
+
 // =======================
 // routes ================
 // =======================
@@ -54,6 +57,33 @@ app.get('/setup', function(req, res) {
     res.json({ success: true });
   });
 });
+
+
+var apiRoutes = express.Router(); 
+
+// API ROUTES -------------------
+
+// get an instance of the router for api routes
+var apiRoutes = express.Router(); 
+
+// TODO: route to authenticate a user (POST http://localhost:8080/api/authenticate)
+
+// TODO: route middleware to verify a token
+
+// route to show a random message (GET http://localhost:8080/api/)
+apiRoutes.get('/', function(req, res) {
+  res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+
+// route to return all users (GET http://localhost:8080/api/users)
+apiRoutes.get('/users', function(req, res) {
+  User.find({}, function(err, users) {
+    res.json(users);
+  });
+});   
+
+// apply the routes to our application with the prefix /api
+app.use('/api', apiRoutes);
 
 
 // =======================
